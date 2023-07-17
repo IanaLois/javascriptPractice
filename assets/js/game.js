37854fc9b412a7a14bef49ecaf850df779264d40
IanaLois <>
2023-07-17 17:06:08 +0000

    Rewrite box id to make it more legible

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,7 @@
 function drawBox(container, row, col, letter = '') {
     const box = document.createElement('div');
     box.className = 'box';
-    box.id = `box${row}${col}`;
+    box.id = 'box' + '_' + row + '_' + col;
     box.textContent = letter;
 
     container.appendChild(box);
